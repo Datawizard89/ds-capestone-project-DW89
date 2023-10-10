0cffb6444d9de19a7646f7eaeae285f2363ace8e
Mojtaba Peyrovi <mojtaba.peyrovi@gmail.com>
2023-10-10 20:44:16 +0200

    added the GCS integration module

---
+++ app/gcs_integration.py
@@ -0,0 +1,32 @@
+from google.cloud import storage
+
+def read_data_from_gcs(bucket_name, object_name):
+    """Read data from Google Cloud Storage."""
+    try:
+        # Create a client using Application Default Credentials (ADC)
+        storage_client = storage.Client()
+
+        # Get the bucket and object
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(object_name)
+
+        # Download the data
+        data = blob.download_as_text()
+
+        return data
+
+    except Exception as e:
+        print(f"Error reading data from GCS: {e}")
+        return None
+
+# Example usage
+if __name__ == "__main__":
+    bucket_name = "your-bucket-name"
+    object_name = "path/to/your/object.txt"
+    
+    data = read_data_from_gcs(bucket_name, object_name)
+    
+    if data:
+        print(f"Data from GCS: {data}")
+    else:
+        print("Failed to read data from GCS.")
