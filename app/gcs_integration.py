330ad876b732ddd2d0a5ccddbe1cbf480412a88c
Mojtaba Peyrovi <mojtaba.peyrovi@gmail.com>
2023-10-12 12:07:33 +0200

    image generation py file based on google map API done

--- app/gcs_integration.py
+++ app/gcs_integration.py
@@ -1,5 +1,5 @@
 from google.cloud import storage
-from io import BytesIO
+from io import BytesIO, StringIO
 from PIL import Image
 import numpy as np
 
@@ -38,6 +38,77 @@ def read_image_from_gcs(bucket_name, blob_name, keyfile):
 
     
 
+def read_text_file_from_gcs(bucket_name, file_name, json_key_path):
+    """
+    Read a text file from Google Cloud Storage using a service account JSON key.
+    """
+    try:
+        # Initialize the Google Cloud Storage client with the service account JSON key.
+        client = storage.Client.from_service_account_json(json_key_path)
+
+        # Get a reference to the specified bucket and file.
+        bucket = client.bucket(bucket_name)
+        blob = bucket.blob(file_name)
+
+        # Download the content of the text file.
+        text_content = blob.download_as_text()
+
+        return text_content
+    except Exception as e:
+        print(f"Error reading text file from GCS: {e}")
+        return None
+
+
+
+
+
+from google.cloud import storage
+import pandas as pd
+
+def download_csv_as_dataframe(bucket_name, blob_name, json_key_path):
+    """
+    Download a CSV file from Google Cloud Storage and return it as a Pandas DataFrame.
+    """
+    try:
+        # Initialize the Google Cloud Storage client with the service account JSON key.
+        client = storage.Client.from_service_account_json(json_key_path)
+
+        # Get a reference to the specified bucket and blob.
+        bucket = client.bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+
+        # Download the CSV file as a string.
+        csv_data = blob.download_as_text()
+
+        # Convert the CSV data to a Pandas DataFrame.
+        df = pd.read_csv(StringIO(csv_data))
+
+        return df
+    except Exception as e:
+        print(f"Error downloading CSV file and creating DataFrame: {e}")
+        return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -70,6 +141,49 @@ def write_dataframe_to_gcs(dataframe, bucket_name, blob_name, json_key_path):
 
 
 
+
+
+
+
+
+
+def write_bytes_to_gcs_as_image(bucket_name, blob_name, image_bytes, json_key_path):
+    """
+    Write image bytes to Google Cloud Storage as an image file using a service account JSON key.
+    """
+    try:
+        # Initialize the Google Cloud Storage client with the service account JSON key.
+        client = storage.Client.from_service_account_json(json_key_path)
+
+        # Get a reference to the specified bucket and create the blob (image) with the given content.
+        bucket = client.bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+
+        # Upload the image bytes to the specified blob in GCS.
+        blob.upload_from_string(image_bytes, content_type="image/jpeg")  # Adjust content_type if needed.
+
+        return True
+    except Exception as e:
+        print(f"Error writing image to GCS: {e}")
+        return False
+
+
+
+
+# Example usage:
+if __name__ == "__main__":
+    # Replace these values with your own.
+    blob_name = "data/sample_iamges/map_image.png"
+
+    # Read the image as bytes (for example, from a local file).
+    with open("path/to/your/local/image.jpg", "rb") as f:
+        image_data = f.read()
+
+    # Write the image bytes to Google Cloud Storage.
+    success = write_bytes_to_gcs_as_image(bucket_name, blob_name, image_data, json_key_path)
+
+    if success:
+        print("Image written to GCS successfully.")
 
 
 
--- app/get_address_image.py
+++ app/get_address_image.py
@@ -1,9 +1,48 @@
-
+from gcs_integration import read_text_file_from_gcs, download_csv_as_dataframe, write_bytes_to_gcs_as_image
 import requests
 import pandas as pd
 import os 
 import numpy as np 
 
 
-api_key = "AIzaSyAizfQGtbFRW4qjTfheqkNxANqbFk0lxe0"
+
+api_file = "data/StaticKey.txt"
+json_keyfile_path = '../galvanic-fort-399815-b650c8e36bea.json'
+bucket_name = 'datasolamente'
+
+
+
+
+google_map_static_api_key = read_text_file_from_gcs(bucket_name, api_file, json_keyfile_path)
+
+
+addresses_df = download_csv_as_dataframe(bucket_name, "data/address_lat_long.csv", json_keyfile_path)
+BASE_URL = "https://maps.googleapis.com/maps/api/staticmap?"
+
+print(addresses_df.head())
+
+def get_google_images(addresses_df):
+    addresses_df['center'] = addresses_df['address'].str.replace(' ', '+')
+    center_list = addresses_df.center.to_list()
+
+    for item in center_list:
+        satelite_img_url = BASE_URL + "center=" + item + "&zoom=" + \
+            str(20) + "&size=400x400&maptype=satellite&key=" + google_map_static_api_key
+        map_img_url = BASE_URL + "center=" + item + "&zoom=" + \
+            str(20) + "&size=400x400&maptype=roadmap&key=" + google_map_static_api_key
+
+        satelite_img = requests.get(satelite_img_url)    
+        map_img = requests.get(map_img_url) 
+        
+        return map_img.content
+        # print(type(map_img.content))
+
+        break
+        
+
+sample_img = get_google_images(addresses_df)   
+
+blob_name = "data/sample_images/map_image.png"
+
+write_bytes_to_gcs_as_image(bucket_name, blob_name, sample_img, json_keyfile_path)
 
